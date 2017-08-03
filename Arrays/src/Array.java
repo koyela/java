073ddeb3362cc3622fe8ff35ccc4de3d89c29e44
koyela <koyelaprovidence2@gmail.com>
2017-08-03 13:35:22 +0100

    Add files via upload

---
+++ Arrays/src/Array.java
@@ -0,0 +1,17 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = new String[4];
+		names[0] = "john";
+		names[1] = "mary";
+		names[2] = "lambert";
+		System.out.println(names[2]);
+		
+		String[] house = {"Home","office","school"};
+		System.out.println(house[2]);
+
+	}
+
+}
---
+++ Variables&Operators/src/variables.java
@@ -0,0 +1,31 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int score , salary , murgage , Bills;
+		score = 6 + 10;
+		salary = 200 + 300;
+		murgage = 5000 + 1;
+		Bills = 200 + 500;
+		System.out.println(score);
+		System.out.println(salary);
+		System.out.println(murgage);
+		System.out.println(Bills);
+		
+		String firstName , lastName , houseAddress , region;
+		firstName = "Providence";
+		lastName = "Lambert";
+		houseAddress = "Synod Office";
+		region = "Buea";
+		
+				
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(houseAddress);
+		System.out.println(region);
+
+	}
+
+}
